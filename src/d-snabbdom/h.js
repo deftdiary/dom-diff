64eb4531db35f94e3ca568fe15e81fa9e5b60b74
deft <deftmail@163.com>
2021-01-04 16:17:29 +0800

    my h vnode

---
+++ src/d-snabbdom/h.js
@@ -0,0 +1,26 @@
+import vnode from './vnode'
+
+export default function (sel, data, c) {
+  if (arguments.length !== 3) {
+    throw new Error('require 3 paramters')
+  }
+  if (typeof c === 'string' || typeof c === 'number') {
+    return vnode(sel, data, undefined, c, undefined)
+  } else if (Array.isArray(c)) {
+    // arr of h function
+    let children = []
+    for (let i = 0; i < c.length; i++) {
+      if (!(typeof c[i] === 'object' && c[i].hasOwnProperty('sel'))) {
+        throw Error('param should be h function')
+      }
+      children.push(c[i])
+    }
+    return vnode(sel, data, children, undefined, undefined)
+  } else if (typeof c === 'object' && c.hasOwnProperty('sel')) {
+    // single h function
+    let children = [c]
+    return vnode(sel, data, children, undefined, undefined)
+  } else {
+    throw new TypeError('type of the third parameter is not correct')
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import { init } from 'snabbdom/init'
-import { classModule } from 'snabbdom/modules/class'
-import { propsModule } from 'snabbdom/modules/props'
-import { styleModule } from 'snabbdom/modules/style'
-import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
-import { h } from 'snabbdom/h' // helper function for creating vnodes
+import h from './d-snabbdom/h'
 
-// patch
-var patch = init([classModule, propsModule, styleModule, eventListenersModule])
-
-// create vnode
-// var myVnode1 = h('a', { props: { href: 'http://www.baidu.com', target: '_blank' } }, '百度')
-// console.log(myVnode1) // df-log
-var myVnode2 = h('ul', {}, [h('li', 'monkey'), h('li', 'pig'), h('li', 'hourse')] )
-
-// render vnode to dom
-var container = document.getElementById('container')
-patch(container, myVnode2)
+var myVnode1 = h('div', {}, [
+  h('div', {}, 'hello'),
+  h('div', {}, 'hello'),
+  h('div', {}, h('div', {}, 'hello'))
+])
+console.log(myVnode1)
